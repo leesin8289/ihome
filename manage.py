3fd3ef8e30492b3a6a9a0b0d503c4d26cb2ac0bd
leesin <616148900@qq.com>
2018-05-13 09:58:22 +0800

    2.mysql

--- manage.py
+++ manage.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 class Config(object):
     DEBUG = True
 
+    # mysql配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
