a63869950410a8ed7adbeeb6d74ab87411b2ae8f
leesin <616148900@qq.com>
2018-05-13 09:52:27 +0800

    1.git项目管理

---
+++ manage.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+class Config(object):
+    DEBUG = True
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
